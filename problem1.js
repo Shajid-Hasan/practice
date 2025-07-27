9722ad8d39c294f5d2baafd873bd7419b5f0d5f0
Shajid <hasan.shajid222@gmail.com>
2025-07-27 12:55:38 +0600

    first commit

---
+++ problem1.js
@@ -0,0 +1,7 @@
+var money = 1000;
+var orangePrice = 400;
+var applePrice = 300;
+
+var price = orangePrice + applePrice;
+price -= 1000;
+console.log(price);
---
+++ problem2.js
@@ -0,0 +1,10 @@
+
+var mathematics = 72.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.50;
+
+averageMarks = (mathematics + biology + chemistry + physics + bangla) / (5);
+
+console.log(averageMarks.toFixed(2));
