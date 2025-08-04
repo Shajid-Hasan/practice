c00a21d779858c9cff3fb5713f574d0b902edd9e
Shajid <hasan.shajid222@gmail.com>
2025-08-04 12:38:04 +0600

    added new

---
+++ problem6.js
@@ -0,0 +1,6 @@
+const fruits = ['apple', 'orrange', 'banana', 'jackfruit', 'watermelon'];
+console.log(fruits);
+console.log(fruits[3]);
+fruits[2] = "Jackfruit";
+console.log(fruits);
+
---
+++ problem7.js
@@ -0,0 +1,7 @@
+
+const place = ['cox-bazar', 'Sajek', 'kuakata'];
+console.log(place);
+place.push("Bandarban");
+console.log(place);
+place.pop();
+console.log(place);
